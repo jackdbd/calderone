d81eda72aac1f90fd5406794db86215dad4a6fba
Giacomo Debidda <giacomo@giacomodebidda.com>
2022-03-08 13:46:38 +0100

    chore(root,checks,schemas): add config for commitlint and semantic-release

---
+++ config/semantic-release.cjs
@@ -0,0 +1,53 @@
+/**
+ * configuration for semantic-release and multi-semantic-release
+ *
+ * multi-semantic-release requires a semantic release config to be present in
+ * EACH package or/and declared in repo root.
+ * This config file is executed in EACH package root, not in the monorepo root.
+ */
+
+const commitlint_config_path = '../../commitlint.cjs'
+
+// https://github.com/semantic-release/changelog
+const changelog = [
+  '@semantic-release/changelog',
+  {
+    changelogFile: 'CHANGELOG.md',
+    changelogTitle: '# CHANGELOG'
+  }
+]
+
+// https://github.com/semantic-release/commit-analyzer
+// I prefer to keep the configuration for the commit linter in
+// config/commitlint.cjs, so I can run npm run lint even when I am not releasing
+// (I like to lint commits with a pre-push git hook).
+// Since in config/commitlint.cjs I am using conventional commits, these 2
+// configurations for @semantic-release/commit-analyzer are equivalent:
+// 1. config: './config/commitlint.cjs'
+// 2. preset: 'conventionalcommits'
+const commit_analyzer = [
+  '@semantic-release/commit-analyzer',
+  {
+    config: commitlint_config_path
+  }
+]
+
+// https://github.com/semantic-release/release-notes-generator
+const release_notes_generator = [
+  '@semantic-release/release-notes-generator',
+  {
+    config: commitlint_config_path
+  }
+]
+
+const config = {
+  // https://semantic-release.gitbook.io/semantic-release/usage/configuration#branches
+  branches: ['main', 'release'],
+  ci: true,
+  // ci: false,
+  // The git plugin must be called AFTER the npm plugin. See here:
+  // https://github.com/semantic-release/git#examples
+  plugins: [commit_analyzer, release_notes_generator, changelog]
+}
+
+module.exports = config
---
+++ config/utils.cjs
@@ -0,0 +1,14 @@
+/**
+ * Message to be used by the semantic-release git plugin when creating a release
+ * commit. It's defined here so all packages use a consistent release message.
+ */
+const releaseMessage = () => {
+  return 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+}
+
+const isPublishingToArtifactRegistry = () => true
+
+module.exports = {
+  isPublishingToArtifactRegistry,
+  releaseMessage
+}
