77d243eaeb75aab7a427504d2a195acfb3573eee
Giacomo Debidda <giacomo@giacomodebidda.com>
2022-05-13 18:19:12 +0200

    fix(checks): update semantic-release config

--- packages/checks/release.config.cjs
+++ packages/checks/release.config.cjs
@@ -1,6 +1,9 @@
 const base_config = require('../../config/semantic-release.cjs')
-const { git, github } = require('../../config/semantic-release-plugins.cjs')
-// const { isPublishingToArtifactRegistry } = require('../../config/utils.cjs')
+const { github } = require('../../config/semantic-release-plugins.cjs')
+
+// https://github.com/semantic-release/git#message
+const RELEASE_COMMIT_MESSAGE =
+  'chore(checks): release v.${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 
 // let config = {}
 // if (isPublishingToArtifactRegistry()) {
@@ -21,13 +24,26 @@ const { git, github } = require('../../config/semantic-release-plugins.cjs')
 //   }
 // }
 
+// The git plugin must be called AFTER the npm plugin. See here:
+// https://github.com/semantic-release/git#examples
+const git = [
+  '@semantic-release/git',
+  {
+    assets: ['CHANGELOG.md', 'package.json'],
+    message: RELEASE_COMMIT_MESSAGE
+  }
+]
+
+// https://github.com/semantic-release/npm
+const npm = ['@semantic-release/npm', { npmPublish: false, pkgRoot: './lib' }]
+
 const config = {
   ...base_config,
   ci: true,
   // The git plugin must be called AFTER the npm plugin. See here:
   // https://github.com/semantic-release/git#examples
-  // plugins: [...base_config.plugins, npm, github, git]
-  plugins: [...base_config.plugins, github, git]
+  // https://semantic-release.gitbook.io/semantic-release/support/faq#why-is-the-package.jsons-version-not-updated-in-my-repository
+  plugins: [...base_config.plugins, npm, github, git]
 }
 
 module.exports = config
