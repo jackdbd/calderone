3ee231ad0153209991858ff69c46f8810794ff3d
Giacomo Debidda <giacomo@giacomodebidda.com>
2022-05-13 18:30:10 +0200

    chore: cleanup semantic-release config

--- packages/hapi-healthcheck-plugin/release.config.cjs
+++ packages/hapi-healthcheck-plugin/release.config.cjs
@@ -1,55 +1,30 @@
 const base_config = require('../../config/semantic-release.cjs')
-const {
-  isPublishingToArtifactRegistry,
-  releaseMessage
-} = require('../../config/utils.cjs')
+const { github } = require('../../config/semantic-release-plugins.cjs')
 
-// https://github.com/semantic-release/github
-// I am not using the github plugin when publishing on Artifact Registry because
-// I use Artifact Registry to test the release process, and I don't want to create
-// a GitHub release when doing it.
-const github = [
-  '@semantic-release/github',
-  {
-    assets: [
-      { path: 'README.md' },
-      { path: 'LICENSE' },
-      { path: 'CHANGELOG.md' }
-    ]
-  }
-]
-
-// https://github.com/semantic-release/npm
-const npm = [
-  '@semantic-release/npm',
-  { npmPublish: true, pkgRoot: './to-publish' }
-]
+// https://github.com/semantic-release/git#message
+const RELEASE_COMMIT_MESSAGE =
+  'chore(hapi-healthcheck-plugin): release v.${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 
 // The git plugin must be called AFTER the npm plugin. See here:
 // https://github.com/semantic-release/git#examples
 const git = [
   '@semantic-release/git',
   {
-    assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-    message: releaseMessage()
+    assets: ['CHANGELOG.md', 'package.json'],
+    message: RELEASE_COMMIT_MESSAGE
   }
 ]
 
-let config = {}
-if (isPublishingToArtifactRegistry()) {
-  config = {
-    ...base_config,
-    ci: false,
-    // plugins: [...base_config.plugins, npm]
-    plugins: [...base_config.plugins]
-  }
-} else {
-  config = {
-    ...base_config,
-    ci: true,
-    // plugins: [...base_config.plugins, npm, github, git]
-    plugins: [...base_config.plugins, github, git]
-  }
+// https://github.com/semantic-release/npm
+const npm = ['@semantic-release/npm', { npmPublish: false, pkgRoot: './lib' }]
+
+const config = {
+  ...base_config,
+  ci: true,
+  // The git plugin must be called AFTER the npm plugin. See here:
+  // https://github.com/semantic-release/git#examples
+  // https://semantic-release.gitbook.io/semantic-release/support/faq#why-is-the-package.jsons-version-not-updated-in-my-repository
+  plugins: [...base_config.plugins, npm, github, git]
 }
 
 module.exports = config
