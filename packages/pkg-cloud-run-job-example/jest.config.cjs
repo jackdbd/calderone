93a6671467d3cd1e5982fb458c76cffd159cecd1
Giacomo Debidda <giacomo@giacomodebidda.com>
2022-09-09 18:56:12 +0200

    feat(pkg-cloud-run-job-example): add package

---
+++ packages/pkg-cloud-run-job-example/jest.config.cjs
@@ -0,0 +1,27 @@
+const config = {
+  // https://jestjs.io/docs/configuration#globals-object
+  globals: {},
+
+  testMatch: [`<rootDir>/__tests__/**/*.test.{js,mjs}`],
+
+  // 5s is the default value for slowTestThreshold.
+  // https://jestjs.io/docs/configuration#slowtestthreshold-number
+  slowTestThreshold: 5,
+
+  // https://jestjs.io/docs/configuration#testenvironment-string
+  testEnvironment: 'node',
+
+  // jest-circus/runner is the default value for testRunner.
+  // https://jestjs.io/docs/configuration#testrunner-string
+  testRunner: 'jest-circus/runner',
+
+  // 5000ms is the default value for testTimeout.
+  // https://jestjs.io/docs/configuration#testtimeout-number
+  testTimeout: 5000,
+
+  transform: {}
+}
+
+// console.log('=== Jest config ===', config)
+
+module.exports = config
