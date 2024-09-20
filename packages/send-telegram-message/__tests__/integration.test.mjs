7074c72a9d64ca3ef5e333335b10b58026bce1de
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-09-20 10:44:32 +0200

    test(send-telegram-message): add test suite name in Telegram message

--- packages/send-telegram-message/__tests__/integration.test.mjs
+++ packages/send-telegram-message/__tests__/integration.test.mjs
@@ -1,12 +1,18 @@
+import * as path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import supertest from 'supertest'
 import { getTestServer } from '@google-cloud/functions-framework/testing'
 // https://github.com/GoogleCloudPlatform/functions-framework-nodejs/blob/master/src/testing.ts
 // we need to import the function, so functions-framework/testing can register it
 import * as _ from '../index.js'
 
-const FUNCTION_NAME = 'send-telegram-message'
+const __filename = fileURLToPath(import.meta.url)
+const script_name = path.basename(__filename)
 
-describe(`integration tests for ${FUNCTION_NAME}`, () => {
+const FUNCTION_NAME = 'send-telegram-message'
+const TEST_SUITE_NAME = `integration tests for Cloud Function ${FUNCTION_NAME}`
+
+describe(TEST_SUITE_NAME, () => {
   let server
   beforeAll(() => {
     server = getTestServer(FUNCTION_NAME)
@@ -28,7 +34,9 @@ describe(`integration tests for ${FUNCTION_NAME}`, () => {
       .post('/')
       .set('Content-Type', 'application/json')
       // https://core.telegram.org/bots/api#formatting-options
-      .send({ text: '<b>Hello</b> from <code>Jest</code>' })
+      .send({
+        text: `Hello from <b>Jest</b>!\n\nSent by <i>${TEST_SUITE_NAME}</i> (repo: <code>calderone</code>, script: <code>${script_name}</code>)`
+      })
 
     expect(response.statusCode).toBe(200)
     expect(response.ok).toBeTruthy()
